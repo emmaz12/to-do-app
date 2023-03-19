4e3bb7ad4abefccc3c3f4813dd48feeba29cc2f6
emmaz12 <ezc7288@gmail.com>
2023-03-19 15:52:44 -0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import styles from './styles/App.module.css';
 import Input from './components/input';
-import SubmitButton from './components/submitButton';
+import tastSubmit from './components/tastSubmit';
 
 
 const App = () => {
     return (
         <>
+        <div className = {styles.header}>
+            My To-Do List
+        </div>
+
         <div className={styles.main}>
             <div className = {styles.inputField}>
                 <Input/>
             </div>
         </div>
-
-        <div className = {styles.buttonPosition}>
-            <SubmitButton/>
-        </div>
-
         </>
     );
 };
--- client/src/components/input.tsx
+++ client/src/components/input.tsx
@@ -2,12 +2,39 @@ import React, {useState} from 'react';
 import styles from '../styles/input.module.css';
 
 const Input = () => {
+ 
+    const [text, setText] = useState('');
+    const [tagText, setTagText] = useState('');
+    const [list, setList] = useState<string[]>([]);
+
+    const changeTag = (newText:string) => {
+        setList([...list, newText]);
+    }
 
     return (
         <>
-            <input className={styles.input} type="text" placeholder="I need to..."/>
-            
+        <div className = {styles.input}>
+            <form onSubmit={(e) => {e.preventDefault(); setTagText(text); changeTag(text)}}>
+
+                <input className={styles.text} type="text" placeholder="  I need to..." value = {text} onChange = {(e) => setText(e.target.value)}/>
+                <button className={styles.submitButton}> </button>
+
+            </form>
+        </div>
+
+        <br></br>
+
+        {list.map((value, key) => {
+            return (
+    
+                <div className = {styles.task}>
+                    {value}
+                </div>
+            )
+        })}
         </>
+            
+   
     );
 };
 
